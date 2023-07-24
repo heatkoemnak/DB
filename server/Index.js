4af056c1c58f54cb8952708896823e042ddcf608
Heat Koemnak <100059828+heatkoemnak@users.noreply.github.com>
2023-07-24 10:24:48 +0000

    create structure data for server

--- server/Index.js
+++ server/Index.js
@@ -1,2 +1,12 @@
 
-console.log("Hello world!");
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
+const app = express();
+const port = 5000;
+
+app.listen(port,()=>{
+    console.log(`server is running on port${port}`);
+})
+
+
